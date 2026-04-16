9de08dfa2b8eb8c68ef5c63193d2be30a4828b58
buzhaoliu <buzhao@wustl.edu>
2026-04-16 15:16:07 -0500

    max_steps_per_video always matches the tasks registered episode length

--- harl/envs/maniskill/maniskill_env.py
+++ harl/envs/maniskill/maniskill_env.py
@@ -53,9 +53,10 @@ class ManiSkillEnv:
         if env_args.get("record_video"):
             from mani_skill.utils.wrappers.record import RecordEpisode
             video_dir = env_args.get("video_dir", f"eval_videos/{env_args['task']}")
+            max_episode_steps = env.unwrapped.max_episode_steps
             env = RecordEpisode(
                 env, output_dir=video_dir, save_trajectory=False,
-                max_steps_per_video=env_args.get("max_episode_steps", 200),
+                max_steps_per_video=max_episode_steps,
                 video_fps=30,
             )
         partial_reset = env_args.get("partial_reset", True)
